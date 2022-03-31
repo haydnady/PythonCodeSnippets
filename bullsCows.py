b36dad08c654e1c7527b9286d9a0b916cbdd1b35
Haydn <haydnady@gmail.com>
2022-03-31 00:15:09 -0400

    Format

--- bullsCows.py
+++ bullsCows.py
@@ -52,26 +52,30 @@ def getHint(secret, guess):
     return str(countC) + "A" + str(len(occurance)) + "B"
 
 
-assert getHint("2962", "7236") == "0A2B"
-assert getHint("11", "01") == "1A0B"
-# assert getHint("0765860239", "5736153483")
-# assert getHint("1807", "7810")
-# assert getHint("1123", "0110")
-# assert getHint("1123", "0111")
-assert getHint("1123", "1111") == "1A1B"
-# assert getHint("1123", "1123")
-# assert getHint("1122", "2211")
-# assert getHint("11", "10")
-# assert getHint("1", "1")
+print("==============>", getHint("2962", "7236"))
+assert "==============>", getHint("2962", "7236") == "0A2B"
 
-# print(getHint("2962", "7236"))
-# print(getHint("11", "01"))
-# print(getHint("0765860239", "5736153483"))
-# print(getHint("1807", "7810"))
-# print(getHint("1123", "0110"))
-# print(getHint("1123", "0111"))
-# print(getHint("1123", "1111"))
-# print(getHint("1123", "1123"))
-# print(getHint("1122", "2211"))
-# print(getHint("11", "10"))
-# print(getHint("1", "1"))
+print("==============>", getHint("11", "01"))
+assert "==============>", getHint("11", "01") == "1A0B"
+# assert "==============>", getHint("0765860239", "5736153483")
+# assert "==============>", getHint("1807", "7810")
+# assert "==============>", getHint("1123", "0110")
+# assert "==============>", getHint("1123", "0111")
+print("==============>", getHint("1123", "0111"))
+assert "==============>", getHint("1123", "1111") == "1A1B"
+# assert "==============>", getHint("1123", "1123")
+# assert "==============>", getHint("1122", "2211")
+# assert "==============>", getHint("11", "10")
+# assert "==============>", getHint("1", "1")
+
+# 
+# 
+# print("==============>", getHint("0765860239", "5736153483"))
+# print("==============>", getHint("1807", "7810"))
+# print("==============>", getHint("1123", "0110"))
+# 
+# print("==============>", getHint("1123", "1111"))
+# print("==============>", getHint("1123", "1123"))
+# print("==============>", getHint("1122", "2211"))
+# print("==============>", getHint("11", "10"))
+# print("==============>", getHint("1", "1"))
