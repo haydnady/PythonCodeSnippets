158c1a5dabd3dc208843be416e5ab72f9b4ec417
Haydn <haydnady@gmail.com>
2022-03-31 03:46:52 -0400

    Working

--- bullsCows.py
+++ bullsCows.py
@@ -30,52 +30,75 @@ def getHint(secret, guess):
     countC = 0
     correctValues = 0
     count = 0
-    occurance = []
+    tempList = ""
     
     for i in secret:
         if i in guess:
 
             # Finds match and location is correct
             if i == guess[count]:
-                # print("-------------------------", count)
                 countC += 1
-                occurance.append(i)
+
+                guess = guess[:guess.index(i)] + "x" + guess[guess.index(i)+1:]
+                tempList += "x"
+
+            else:
+                tempList += i
+
+        else:
+            tempList += i
+
+        count += 1
+
+    count = 0
+    for ii in tempList:
+        if ii in guess:
+
+            print("tempList", tempList, "letter", ii, "guess letter", guess[count], "guess", guess)
+            print("-------------------------", count)
 
             # Finds match and location is NOT correct
-            if i != guess[count] and secret.index(i) != guess.index(i) and i not in occurance:
-                # print("++++++++++++++++++++++++++", count)
+            if ii != guess[count]:
+                guess = guess[:guess.index(ii)] + "x" + guess[guess.index(ii)+1:]
                 correctValues += 1
 
+        count += 1
 
-            count += 1
+    return str(countC) + "A" + str(correctValues) + "B"
 
-    return str(countC) + "A" + str(len(occurance)) + "B"
 
+print("==============>", getHint("1807", "7810"))
+assert getHint("1807", "7810") == "1A3B"
+
+print("==============>", getHint("1123", "0110"))
+assert getHint("1123", "0110") == "1A1B"
+
+print("==============>", getHint("1123", "1111"))
+assert getHint("1123", "1111") == "2A0B"
+
+print("==============>", getHint("1123", "0111"))
+assert getHint("1123", "0111") == "1A1B"
+
+print("==============>", getHint("1123", "1123"))
+assert getHint("1123", "1123") == "4A0B"
+
+print("==============>", getHint("1122", "2211"))
+assert getHint("1122", "2211") == "0A4B"
 
 print("==============>", getHint("2962", "7236"))
 assert getHint("2962", "7236") == "0A2B"
 
 print("==============>", getHint("11", "01"))
 assert getHint("11", "01") == "1A0B"
-# assert getHint("0765860239", "5736153483")
-# assert getHint("1807", "7810")
-# assert getHint("1123", "0110")
-# assert getHint("1123", "0111")
-print("==============>", getHint("1123", "0111"))
-assert getHint("1123", "1111") == "1A1B"
-# assert getHint("1123", "1123")
-# assert getHint("1122", "2211")
-# assert getHint("11", "10")
-# assert getHint("1", "1")
 
-# 
-# 
-# print("==============>", getHint("0765860239", "5736153483"))
-# print("==============>", getHint("1807", "7810"))
-# print("==============>", getHint("1123", "0110"))
-# 
-# print("==============>", getHint("1123", "1111"))
-# print("==============>", getHint("1123", "1123"))
-# print("==============>", getHint("1122", "2211"))
-# print("==============>", getHint("11", "10"))
-# print("==============>", getHint("1", "1"))
+print("==============>", getHint("11", "10"))
+assert getHint("11", "10") == "1A0B"
+
+print("==============>", getHint("12", "23"))
+assert getHint("12", "23") == "0A1B"
+
+print("==============>", getHint("1", "1"))
+assert getHint("1", "1") == "1A0B"
+
+print("==============>", getHint("0765860239", "5736153483"))
+assert getHint("0765860239", "5736153483") == "1A4B"
