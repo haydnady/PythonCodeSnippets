fd4c0011c1046638ce21ff87108618865a95e687
Haydn <haydnady@gmail.com>
2022-04-28 19:28:59 -0400

    Optimized solution

--- Algorithms/Two Sum.py
+++ Algorithms/Two Sum.py
@@ -4,21 +4,17 @@ def twoSum(nums, target):
         :type target: int
         :rtype: List[int]
         """
-        result = 0
-        output = []
         
         for idx, val in enumerate(nums):
-            if val > target:
+            if val > target and val > 0:
                 continue
                 
             for i, v in enumerate(nums):
-                print()
-                if val + v == target and idx != i:
-                    output.append(idx)
-                    output.append(i)
-                    
-                    return output
+                # print(val, v, "---------------->", idx, i, "Target ->", target)
+                if (val + v) == target and idx != i:
+                    return [idx, i]
 
 # Test Cases
 print("Passed!" if twoSum(nums=[2,7,11,15], target=9) == [0,1] else "Failed!")
 print("Passed!" if twoSum(nums=[-1,-2,-3,-4,-5], target=-8) == [2,4] else "Failed!")
+print("Passed!" if twoSum(nums=[-3,4,3,90], target=0) == [0,2] else "Failed!")
