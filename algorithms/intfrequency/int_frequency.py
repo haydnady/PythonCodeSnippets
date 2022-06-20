8a70f52a255069df684b491f0f573d4d1c73b272
Haydn <haydnady@gmail.com>
2022-06-19 20:11:09 -0400

    Fixed bug and updated freq_count

--- algorithms/intfrequency/int_frequency.py
+++ algorithms/intfrequency/int_frequency.py
@@ -1,22 +1,22 @@
 
 def freq_count(list_: list, number: int) -> list:
     dict_ = {}
-    solution = []
     level = -1
 
-    for char in str(list_):
+    for char in str(list_):  # Note list to string
         if char.isdigit():
             if int(char) == number:
-                print("---------------", dict_[level])
                 dict_[level] += 1
         elif char == "[":
             level += 1
-            dict_[level] = 0  # Initialize key for this level in dictionary
+            if level not in dict_:
+                dict_[level] = 0  # Initialize key for this level in dictionary
         elif char == "]":
             level -= 1
 
-    print(dict_)
-    return solution
+    # Using list comprehension to convert dictionary to list
+    # print([[k, v] for k, v in dict_.items()])
+    return [[k, v] for k, v in dict_.items()]
 
 
 if __name__ == "__main__":
